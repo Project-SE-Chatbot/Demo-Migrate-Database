e7668a335a54948564ecfa9d231fab5269c9b30d
Qaizx <kaothom@gmail.com>
2023-01-27 01:40:48 +0700

    import module

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,12 +19,24 @@ import { Q_And_A } from './typeorm/entities/back_end/Q&A';
 import { Register } from './typeorm/entities/back_end/Register';
 import { Teacher } from './typeorm/entities/back_end/Teacher';
 import { dataSourceOptions } from 'db/data-source';
+import { DegreeController } from './api/degree/degree.controller';
+import { DegreeModule } from './api/degree/degree.module';
+import { FaqController } from './api/faq/faq.controller';
+import { FaqModule } from './api/faq/faq.module';
+import { FeedBackModule } from './api/feed-back/feed-back.module';
+import { MajorModule } from './api/major/major.module';
+import { PlaceModule } from './api/place/place.module';
+import { PlanModule } from './api/plan/plan.module';
+import { QaModule } from './api/qa/qa.module';
+import { RegisterModule } from './api/register/register.module';
+import { TeacherModule } from './api/teacher/teacher.module';
+import { AdminModule } from './api/admin/admin.module';
 
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
-  controllers: [AppController],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule, DegreeModule, FaqModule, FeedBackModule, MajorModule, PlaceModule, PlanModule, QaModule, RegisterModule, TeacherModule, AdminModule],
+  controllers: [AppController, DegreeController, FaqController],
   providers: [AppService],
 })
 export class AppModule {
