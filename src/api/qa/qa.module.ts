94b24e20d0504ae6be5821420755bc0c2fb977ae
Qaizx <kaothom@gmail.com>
2023-02-06 11:56:01 +0700

    import module

--- src/api/qa/qa.module.ts
+++ src/api/qa/qa.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Q_And_A } from 'src/typeorm/entities/back_end/Q&A';
 import { QaController } from './qa.controller';
 import { QaService } from './qa.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Q_And_A])],
   controllers: [QaController],
   providers: [QaService]
 })
