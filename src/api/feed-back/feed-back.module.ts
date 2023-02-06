4f5dcb3af7c0ec08bbf06b71793788b25979c55b
Qaizx <kaothom@gmail.com>
2023-02-06 11:54:39 +0700

    import module

--- src/api/feed-back/feed-back.module.ts
+++ src/api/feed-back/feed-back.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Feedback } from 'src/typeorm/entities/back_end/Feed Back';
 import { FeedBackController } from './feed-back.controller';
 import { FeedBackService } from './feed-back.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Feedback])],
   controllers: [FeedBackController],
   providers: [FeedBackService]
 })
