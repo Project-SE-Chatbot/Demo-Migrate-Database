c6c8a9eb58965359051b891f0241a83f28ff3585
Qaizx <kaothom@gmail.com>
2023-02-06 12:34:43 +0700

    course_code and name

--- src/api/major/DTO/updateMajor.dto.ts
+++ src/api/major/DTO/updateMajor.dto.ts
@@ -1,8 +1,9 @@
 export class updateMajorDTO {
-    name_teacher: string;
-    term: string;
-    place: string;
-    day: string;
-    time: string;
-  }
-  
+  course_code: string;
+  name: string;
+  name_teacher: string;
+  term: string;
+  place: string;
+  day: string;
+  time: string;
+}
