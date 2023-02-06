d4e66a07e289b8c4bef10873b8ea4544d5c260e2
Qaizx <kaothom@gmail.com>
2023-02-06 12:34:12 +0700

    update free_elective module

--- src/api/major/major.module.ts
+++ src/api/major/major.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Free_Elective } from 'src/typeorm/entities/back_end/Free Elective';
 import { Major } from 'src/typeorm/entities/back_end/Major';
 import { Place } from 'src/typeorm/entities/back_end/Place';
 import { MajorController } from './major.controller';
 import { MajorService } from './major.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Major, Place])],
+  imports: [TypeOrmModule.forFeature([Major, Place, Free_Elective])],
   controllers: [MajorController],
   providers: [MajorService]
 })
