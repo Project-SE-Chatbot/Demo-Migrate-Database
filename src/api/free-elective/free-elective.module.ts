71e2b8f050d4b77e94f10547b1e220b606e2c29b
Qaizx <kaothom@gmail.com>
2023-02-06 11:54:56 +0700

    import module

--- src/api/free-elective/free-elective.module.ts
+++ src/api/free-elective/free-elective.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Free_Elective } from 'src/typeorm/entities/back_end/Free Elective';
 import { FreeElectiveController } from './free-elective.controller';
 import { FreeElectiveService } from './free-elective.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Free_Elective])],
   controllers: [FreeElectiveController],
   providers: [FreeElectiveService]
 })
