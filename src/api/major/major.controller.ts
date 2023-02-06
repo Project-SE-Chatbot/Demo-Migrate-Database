8398c0d74eb60aabe2607b3a8488641301376422
Qaizx <kaothom@gmail.com>
2023-02-06 12:36:16 +0700

    review and find by name or corse_code

--- src/api/major/major.controller.ts
+++ src/api/major/major.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, ParseIntPipe } from '@nestjs/common';
 import { Body, Delete, Param, Post, Put } from '@nestjs/common/decorators';
+import { Free_ElectiveDTO } from '../free-elective/DTO/elective.dto';
 import { PlaceDTO } from '../place/DTO/place.dto';
+import { findNameDTO } from './DTO/findName.dto';
 import { MajorDTO } from './DTO/major.dto';
 import { updateMajorDTO } from './DTO/updateMajor.dto';
 import { MajorService } from './major.service';
@@ -25,6 +27,12 @@ export class MajorController {
         this.majorService.createPlace(id, createPlace)
     }
 
+    @Post(':id/review')
+    createReview(@Param('id', ParseIntPipe) id: number,
+    @Body() createReview: Free_ElectiveDTO){
+        this.majorService.createReview(id, createReview)
+    }
+
     @Put(':id')
     editMajor(@Param('id', ParseIntPipe) id: number, @Body() updateMajor: updateMajorDTO ){
         this.majorService.updateMajor(id, updateMajor)
@@ -38,5 +46,15 @@ export class MajorController {
     @Get(':id')
     findMajorByID(@Param('id', ParseIntPipe) id: number){
         this.majorService.findMajorByID(id)
+    }
+
+    @Get(':code')
+    findMajorByCode(@Param('id') id: string){
+        this.majorService.findMajorByCode(id)
+    }
+
+    @Get('place')
+    findMajorByName(@Body() findName: findNameDTO){
+        this.majorService.findMajorByName(findName.name)
     }
 }
--- src/api/major/major.service.ts
+++ src/api/major/major.service.ts
@@ -1,9 +1,10 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { HttpException } from '@nestjs/common/exceptions';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Free_Elective } from 'src/typeorm/entities/back_end/Free Elective';
 import { Major } from 'src/typeorm/entities/back_end/Major';
 import { Place } from 'src/typeorm/entities/back_end/Place';
-import { MajorParam, PlaceParam } from 'src/ultils/types';
+import { ElectiveParam, MajorParam, PlaceParam } from 'src/ultils/types';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -11,10 +12,11 @@ export class MajorService {
   constructor(
     @InjectRepository(Major) private majorRepository: Repository<Major>,
     @InjectRepository(Place) private placeRepository: Repository<Place>,
+    @InjectRepository(Free_Elective) private freeElectRepository: Repository<Free_Elective>,
   ) {}
 
   findMajor() {
-    return this.majorRepository.find({relations: ['Place']});
+    return this.majorRepository.find({relations: ['Place', 'Free_Elective']});
   }
 
   createMajor(majorDetail: MajorParam) {
@@ -33,6 +35,18 @@ export class MajorService {
       return this.majorRepository.save(major);
   }
 
+  async createReview(
+    id: number,
+    createReview: ElectiveParam,
+  ) {
+    const major = await this.majorRepository.findOneBy({ id_major: id });
+    if (!major)
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+
+    const newReview = this.freeElectRepository.create({...createReview, major});
+    return this.freeElectRepository.save(newReview)
+  }
+
   updateMajor(id_major: number, updateMajor: MajorParam) {
     return this.majorRepository.update({ id_major }, { ...updateMajor });
   }
@@ -44,4 +58,13 @@ export class MajorService {
   findMajorByID(id_major: number) {
     return this.majorRepository.findOne({ where: { id_major } });
   }
+
+  findMajorByCode(id: string) {
+    return this.majorRepository.findOne({ where: { course_code: id } });
+  }
+
+  findMajorByName(name: string){
+    return this.majorRepository.findOne({ where: {name: name} })
+  }
+
 }
