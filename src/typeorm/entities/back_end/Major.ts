b4730fc51c3c22c5e2acb36ddab6fd728783a967
winnapat <GGgun2013@gmail.com>
2023-02-13 15:42:26 +0700

    delete validator

--- src/typeorm/entities/back_end/Major.ts
+++ src/typeorm/entities/back_end/Major.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty } from "class-validator";
+
 import {  Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Free_Elective } from "./Free Elective";
 import { Place } from "./Place";
@@ -12,11 +12,11 @@ export class Major{
     id_major: number;
     
     @Column({unique: true})
-    @IsNotEmpty()
+
     course_code: string
 
     @Column({unique: true})
-    @IsNotEmpty()
+
     name: string
 
     @ManyToMany(() => Teacher, (teacher) => teacher.major, {onDelete: "CASCADE", onUpdate: "CASCADE"})
@@ -24,18 +24,18 @@ export class Major{
     teacher: Teacher[];
 
     @Column()
-    @IsNotEmpty()
+
     term: string;
 
     @ManyToOne(() => Place, (place) => place.major, {onDelete: "SET NULL", onUpdate: "CASCADE"})
     place: Place
 
     @Column()
-    @IsNotEmpty()
+
     day  : string;
     
     @Column()
-    @IsNotEmpty()
+
     time  : string;
 
     @OneToMany(() => Free_Elective, (freeElective) => freeElective.major, {onDelete: "CASCADE", onUpdate: "CASCADE"})
