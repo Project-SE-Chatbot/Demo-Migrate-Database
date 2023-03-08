a117d3a9583eb42c5592bf3c144baa31a7208a23
Qaizx <kaothom@gmail.com>
2023-03-08 22:42:35 +0700

    key place

--- src/api/place/place.module.ts
+++ src/api/place/place.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Place } from 'src/typeorm/entities/back_end/Place';
+import { Place_Key } from 'src/typeorm/entities/back_end/Place_Key';
 import { PlaceController } from './place.controller';
 import { PlaceService } from './place.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Place])],
+  imports: [TypeOrmModule.forFeature([Place, Place_Key])],
   controllers: [PlaceController],
   providers: [PlaceService]
 })
